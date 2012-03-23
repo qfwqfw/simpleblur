fe772a4ff9fc8fc46c2085c9aad0256eeb6d9f7b
qfwqfw <qfwqfw@lavabit.com>
2012-03-23 19:43:09 +0059

    updated extension

--- chrome-src/start.js
+++ chrome-src/start.js
@@ -1,7 +1,6 @@
 var link = document.createElement('link'); 
 //link.href = chrome.extension.getURL('main.css'); 
-link.href = 'https://raw.github.com/qfwqfw/newsblur-css/master/simple.css';
-link.id = 'Your Stylish!'; 
+link.href = 'http://qfwqfw.github.com/newsblur-css/simple.css';
 link.type = 'text/css'; 
 link.rel = 'stylesheet'; 
 document.documentElement.insertBefore(link); 
